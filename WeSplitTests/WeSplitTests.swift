7e4b2c1af549ee287b57d3f15d6abe0af734b745
Jeannie <hello@jeannierombough.com>
2020-03-08 17:47:10 -0400

    Initial Commit

---
+++ WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,33 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by JP on 2020-03-08.
+//
+
+import XCTest
+@testable import WeSplit
+
+class WeSplitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
